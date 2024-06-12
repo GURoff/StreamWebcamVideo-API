88781479ed22a74f4b17a32e997a26caa59a226a
Alexandr Gurov <alexandrgurov1@gmail.com>
2024-06-12 17:24:01 +0200

    added changes for text detecting and imaged display

--- lib/text_recognition.py
+++ lib/text_recognition.py
@@ -1,14 +1,19 @@
 import cv2
 import pytesseract
+import threading
+import time
 
 class RealTimeTextRecognition:
     def __init__(self, tesseract_cmd_path):
         # Provide the path to the Tesseract executable
         pytesseract.pytesseract.tesseract_cmd = tesseract_cmd_path
         # Initializing video capture from a webcam
-        self.cap = cv2.VideoCapture(0)
+        self.cap = cv2.VideoCapture(1)
         if not self.cap.isOpened():
             raise Exception("Failed to open webcam")
+
+        self.running = True
+        self.prev_text = None
 
     def __del__(self):
         # Freeing up webcam resource
@@ -17,25 +22,33 @@ class RealTimeTextRecognition:
         cv2.destroyAllWindows()
 
     def capture_and_process_image(self):
-        while True:
+        while self.running:
             ret, frame = self.cap.read()
             if not ret:
                 print("Image capture error")
                 break
 
-            # Flip the image horizontally
-            flipped_frame = cv2.flip(frame, 1)
-
-            # Image preprocessing
-            gray = cv2.cvtColor(flipped_frame, cv2.COLOR_BGR2GRAY)
-            _, thresh = cv2.threshold(gray, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-
-            # Display a preprocessed image for debugging
-            cv2.imshow('Preprocessed Image', thresh)
+            # Converting an Image to Grayscale
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
             # Using Tesseract for OCR
-            text = pytesseract.image_to_string(thresh, config='--psm 6')
+            text = pytesseract.image_to_string(gray, config='--oem 3 --psm 6')
+            boxes = pytesseract.image_to_boxes(gray, config='--oem 3 --psm 6')
 
+            # Drawing rectangles around text
+            h, w = gray.shape
+            for b in boxes.splitlines():
+                b = b.split(' ')
+                x, y, x2, y2 = int(b[1]), int(b[2]), int(b[3]), int(b[4])
+                # Converting coordinates for display on an image
+                cv2.rectangle(frame, (x, h - y), (x2, h - y2), (0, 255, 0), 2)
+
+            # Checking if the current text matches the previous one
+            if text.strip() == self.prev_text:
+                continue
+            # Saving the current text for the next iteration
+            self.prev_text = text.strip()
+            
             # Output of recognized text
             print(f"Recognized text: {text.strip()}")
 
@@ -44,10 +57,12 @@ class RealTimeTextRecognition:
 
             # Quitting when pressing 'q' key
             if cv2.waitKey(1) & 0xFF == ord('q'):
+                self.running = False
                 break
+            
+            # Adding delay
+            time.sleep(0.5)
 
-if __name__ == "__main__":
-    # Provide the path to the Tesseract executable
-    tesseract_cmd_path = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-    recognizer = RealTimeTextRecognition(tesseract_cmd_path)
-    recognizer.capture_and_process_image()
+    def start(self):
+        # Starting a thread to process images
+        threading.Thread(target=self.capture_and_process_image).start()
